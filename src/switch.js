88bd869c02406996495f5052b3890baf4d412a61
dayounglee <verdantjuly@kakao.com>
2023-06-10 06:42:34 +0900

    upOneLove editted

--- src/switch.js
+++ src/switch.js
@@ -1,25 +1,10 @@
-
-
-
 export function mobileSwitch() {
-
-
     let changehref = "./css/mobile.css";
-
-
     let css = document.getElementById("cssline");
-
-
     css.href = changehref;
 }
 export function desktopSwitch() {
-
-
     let changehref = "./css/index.css";
-
-
     let css = document.getElementById("cssline");
-
-
     css.href = changehref;
 }
